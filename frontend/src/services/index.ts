6a73dbca9571ef6bff08ee5eb8eb0d36c010c86b
philipjpark <66290921+philipjpark@users.noreply.github.com>
2026-01-21 12:16:12 -0500

    added craco.config.js file

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -2,3 +2,4 @@
 export { signalService } from './signalService';
 export { minamService } from './minamService';
 export { syuzhetService } from './syuzhetService';
+export { signalAgentOrchestrator } from './signalAgentOrchestrator';
